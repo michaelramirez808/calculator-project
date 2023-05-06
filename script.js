9fb5f1770921f7a208eb39c5efb8f01d3c2573df
Michael <michael76ramirez@gmail.com>
2023-05-05 20:45:36 -0700

    first calculator design complete, no functionality

---
+++ script.js
@@ -0,0 +1,21 @@
+// basic functions
+function add(x, y){
+    return x + y; 
+}
+function subtract(x, y){
+    return x - y;
+}
+function multiply(x, y){
+    return x * y;
+}
+function divide(x, y){
+    return x / y;
+}
+function operate(operator, number1, number2){
+    operator(number1, number2);
+}
+//defining variables
+let operator;
+let number1;
+let number2;
+
